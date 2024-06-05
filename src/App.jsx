84783326d45a269baf25366ecd5bee453ed449f1
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-06-05 13:38:02 +0200

    created the server component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom"; 
 import Home from "./home" 
 import About from "./about"
-import "./server"
 
 function App() {
   return (
@@ -22,31 +21,5 @@ function App() {
   )
 }
 
-// ReactDOM
-// .createRoot(document.getElementById('root'))
-// .render(<App />);
-
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
-
-// function App() {
-
-//   return (
-//     <h1>Start here</h1>
-//   )
-// }
 
 export default App
