c3b2e10cddb3a3659097387c2869261b8ec6c72e
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-06-07 15:30:28 +0200

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -17,19 +17,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path ="/" element={<Layout />}>
-          <Route index element={<Home />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
-
-          <Route path = "vans">
-          <Route index element={<Vans/>} />
+          <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          </Route>
-
+          
           <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
+            <Route path="host" element={<Dashboard />} />
             <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
+            <Route path="host/reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
