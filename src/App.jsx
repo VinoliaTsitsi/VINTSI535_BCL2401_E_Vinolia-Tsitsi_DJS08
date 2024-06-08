e33467abe8d6e9483255ba0ae315bf070615930f
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-06-08 13:06:46 +0200

    Building out the host vans list detail page.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import VanDetail from "./pages/Vans/vanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/Reviews"
+import HostVanDetail from "./pages/Host/HostVanDetail"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 
@@ -24,9 +26,11 @@ function App() {
           <Route path="vans/:id" element={<VanDetail />} />
           
           <Route path="host" element={<HostLayout />}>
-            <Route path="host" element={<Dashboard />} />
+            <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
-            <Route path="host/reviews" element={<Reviews />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
           </Route>
         </Route>
       </Routes>
